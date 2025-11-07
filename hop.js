5330c6289c8fcc364f5e028be60179b0ddb4569a
abirnis120 <abirnishat66@gmail.com>
2025-11-07 23:45:15 +0600

    first commit

---
+++ hop.js
@@ -0,0 +1,11 @@
+// Declare an array with 5 fruits
+let fruits = ["apple", "banana", "mango", "orange", "grape"];
+
+// Console log the element at index 3
+console.log(fruits[3]); // Output: orange
+
+// Change the value at index 2 to "jambura"
+fruits[2] = "jambura";
+
+// Console log the final array
+console.log(fruits);
